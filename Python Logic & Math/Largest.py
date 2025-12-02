88129b859c319033123f73f89dd301d47ef907d7
Laiq Ur Rehman <laiqmufti1019@gmail.com>
2025-12-02 21:18:48 +0500

    Refactor function to find the largest number

--- Python Logic & Math/Largest.py
+++ Python Logic & Math/Largest.py
@@ -1,19 +1,20 @@
-def get_biggest(a, b, c, d):
-    biggest = a
+def find_largest(n1, n2, n3, n4):
+    winner = n1
     
-    if b > biggest:
-        biggest = b
-    if c > biggest:
-        biggest = c
-    if d > biggest:
-        biggest = d
+    if n2 > winner:
+        winner = n2
         
-    return biggest
+    if n3 > winner:
+        winner = n3
+        
+    if n4 > winner:
+        winner = n4
+        
+    return winner
 
-# Get 4 numbers
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter second number: "))
-n3 = int(input("Enter third number: "))
-n4 = int(input("Enter fourth number: "))
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = int(input("Enter number 3: "))
+d = int(input("Enter number 4: "))
 
-print("The largest number is:", get_biggest(n1, n2, n3, n4))
+print("Largest number is:", find_largest(a, b, c, d))
