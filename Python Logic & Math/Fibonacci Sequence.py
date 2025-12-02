dc7a1b222a21cfeffd517e94c66d60f688aac197
Laiq Ur Rehman <laiqmufti1019@gmail.com>
2025-12-02 21:17:39 +0500

    Rename show_fibonacci to fibonacci and refactor

--- Python Logic & Math/Fibonacci Sequence.py
+++ Python Logic & Math/Fibonacci Sequence.py
@@ -1,14 +1,15 @@
-def show_fibonacci(limit):
-    a = 0
-    b = 1
+def fibonacci(limit):
+    first = 0
+    second = 1
     
-    while a <= limit:
-        print(a)
+    while first <= limit:
+        print(first)
         
-        next_val = a + b
+        next_num = first + second
         
-        a = b
-        b = next_val
+        first = second
+        second = next_num
 
-user_limit = int(input("Enter the limit number: "))
-show_fibonacci(user_limit)
+num = int(input("Enter the limit: "))
+
+fibonacci(num)
